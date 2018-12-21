69fd080c198f41e154f5f1fd2fc699432b7e4352
Jeffrey Yunfan Liu <jeffreyliu3@gmail.com>
2018-12-20 23:06:37 -0500

    Initial Commit

---
+++ src/main/java/re/liujeff/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,14 @@
+package re.liujeff.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+}
+
